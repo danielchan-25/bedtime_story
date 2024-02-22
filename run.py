7019be5f87c21120b5e6f05171aee7b42c01ff68
司徒仲豪 <50611800+danielchan-25@users.noreply.github.com>
2024-02-22 11:25:07 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,14 +5,14 @@ import requests
 from PIL import Image
 import io
 import base64
-import googletrans
 import asyncio
-from googletrans import Translator
+from translate import Translator
 import json
 import edge_tts
 import logging
 import subprocess
 import configparser
+import datetime
 
 # --------------------------------- #
 # Date: 2024-02-20
@@ -23,48 +23,53 @@ import configparser
 # 图音合成：FFMpeg
 # --------------------------------- #
 
+logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger()
+now_time = datetime.datetime.today().strftime('%Y-%m-%d_%H%M%S')
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
-logger = logging.getLogger()
+image_dir = config['global']['image_dir']
+audio_dir = config['global']['audio_dir']
+video_dir = config['global']['video_dir']
+video_txt = config['global']['video_txt']
+done_video_dir = config['global']['done_video_dir']
 
 
 # 生成故事内容
 def get_content(prompt):
+    logger.info(f'正在输入Promtps: {prompt}')
     chatglm_api_address = config['global']['chatglm_api_address']
-    headers = {'Content-Type': 'application/json'}
-    payload = {
-        'prompt': f'{prompt}',
-        'history': []
-    }
+    headers = { 'Content-Type': 'application/json' }
+    payload = { 'prompt': f'{prompt}', 'history': [] }
     response = requests.post(url=chatglm_api_address, headers=headers, json=payload)
 
     if response.status_code == 200:
         content = json.loads(response.text)['response']
+        logger.info(f'文章生成成功: {content}')
         return content
 
     else:
-        logger.error(f'内容生成失败,检查下ChatGLM API是否正常')
+        logger.error(f'文章生成失败,检查下ChatGLM API是否正常')
         sys.exit(1)
 
 
 # 内容优化,分段处理
 def content_tuning(content):
+    logger.info(f'开始文章优化')
     content_list = content.split('\n\n', -1)  # 内容分段
+    logger.info('正在对文章进行分段...')
 
     if '睡前故事' in content_list[0]:
-        logger.info(f'正在对内容优化，去除prompts...')
+        logger.info('正在删除多余内容...')
         content_list = content_list[1:]
     return content_list
 
 
 # 文字配音
 def content_dubbing(content):
-    output_dir = 'output\\audio\\'
-    voice = config['edge_tts']['voice']  # 人物音色
+    logger.info('开始生成配音')
+    voice = config['edge_tts']['voice']
 
     if type(content) is list:
         async def save(content, output_file):
@@ -74,7 +79,7 @@ def content_dubbing(content):
         async def save_audio():
             for index, i in enumerate(content):
                 logger.info(f'正在给第{int(index) + 1}句配音...')
-                output_file = f'{output_dir}output_{index}.mp3'
+                output_file = f'{audio_dir}output_{index}.mp3'
                 await save(i, output_file)
 
         asyncio.run(save_audio())
@@ -84,16 +89,22 @@ def content_dubbing(content):
 
 
 # 中译英
-def cn_to_en(word):
-    translater = Translator()
-    output = translater.translate(word, dest='en', src='auto')
-    return output
+def cn_to_en(content):
+    logger.info(f'正在对文章进行翻译...')
+    content_list = []
+    if type(content) is list:
+        for i in content:
+            translator = Translator(from_lang="zh", to_lang="en")
+            translated_text = translator.translate(i)
+            content_list.append(translated_text)
+        return content_list
+    else:
+        logger.error(f'{content}格式错误，应为列表格式')
 
 
 # 文生图
 def transfer_sdapi(prompt):
-    image_dir = 'output\\image\\'
-
+    logger.info('开始生成图片')
     if type(prompt) is list:
         for index, i in enumerate(prompt):
             stablediffusion_api_address = config['global']['stablediffusion_api_address']  # API Address
@@ -115,7 +126,7 @@ def transfer_sdapi(prompt):
             response = requests.post(url=f'{stablediffusion_api_address}/sdapi/v1/txt2img', json=payload)
 
             if response.status_code == 200:
-                logger.info(f'连接 Stable Diffusion API 成功，正在生成第{int(index) + 1}图片...')
+                logger.info(f'正在生成第{int(index) + 1}图片...')
                 r = response.json()
                 image = Image.open(io.BytesIO(base64.b64decode(r['images'][0])))
                 # image.show()
@@ -129,12 +140,6 @@ def transfer_sdapi(prompt):
 
 # 图音合并
 def merge():
-    image_dir = 'output\\image\\'  # 图片路径
-    audio_dir = 'output\\audio\\'  # 音频路径
-    video_dir = 'output\\video\\'  # 单个合成视频路径
-    done_video_dir = 'output\\done_video\\'  # 最终视频路径
-
-
     # 获取音频时长，用于限制视频时长
     def get_audio_time(audio_file):
         command = ['ffprobe', '-i', f'{audio_dir}{audio_file}', '-show_entries', 'format=duration', '-v', 'quiet',
@@ -166,29 +171,40 @@ def merge():
     # 将所有单个视频进行合并
     def merge_all():
         logger.info(f'正在合成所有单个视频...')
-        file = open(f'output\\video.txt', 'w')
-        for i in os.listdir('output\\video\\'):
+        file = open(video_txt, 'w')
+        for i in os.listdir(video_dir):
             file.write('file ' + "video/" + i + '\n')
         file.close()
         command = ['ffmpeg', '-f', 'concat', '-safe', '0', '-i', 'output\\video.txt', '-c', 'copy',
-                   f'{done_video_dir}output.mp4']
+                   f'{done_video_dir}output_{now_time}.mp4']
         subprocess.run(command)
+        logger.info(f'视频合成完毕，具体见：{done_video_dir}')
 
-    audio_list = [i for i in os.listdir('output\\audio\\')]
-    image_list = [i for i in os.listdir('output\\image\\')]
-
+    audio_list = [i for i in os.listdir(audio_dir)]
+    image_list = [i for i in os.listdir(image_dir)]
     for i in range(len(audio_list)):
         merge_video(audio_file=audio_list[i], image_file=image_list[i])
     merge_all()
+
+
+# 删除旧的素材
+def delete_files(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        logger.info(f'正在删除{filename}...')
+        if os.path.isfile(file_path):
+            os.remove(file_path)
 
 
 if __name__ == '__main__':
     prompt = '请生成一个适合0-3岁儿童的睡前故事'
     content = get_content(prompt=prompt)
     content_zhCN = content_tuning(content=content)
-    print(content_zhCN)
-    # content_enUS = cn_to_en(word=content_zhCN)
-    # print(content_zhCN, content_enUS)
-    # transfer_sdapi(prompt=content)
-    # content_dubbing(content=cn_content)
-    # merge()
+    content_enUS = cn_to_en(content=content_zhCN)
+    transfer_sdapi(prompt=content_enUS)
+    content_dubbing(content=content_zhCN)
+    merge()
+    logger.info('开始删除旧素材')
+    delete_files(image_dir)
+    delete_files(audio_dir)
+    delete_files(video_dir)
